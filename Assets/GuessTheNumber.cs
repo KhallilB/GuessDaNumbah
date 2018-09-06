3f77adf28ede90bf802a3e70d713d768d8b08b25
KhallilB <khallil.bailey@gmail.com>
2018-09-06 16:41:12 -0700

    Simple Number Guessing Game

---
+++ Assets/GuessTheNumber.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuessTheNumber : MonoBehaviour {
+
+    public InputField input;
+    public Text infoText;
+
+    private int guessNumber;
+    private int usersGuess;
+
+	// Use this for initialization
+	void Start () {
+        guessNumber = Random.Range(0, 100);
+	}
+	
+    public void CheckGuess() {
+        usersGuess = int.Parse(input.text);
+
+        if (usersGuess == guessNumber){
+            infoText.text = "You Won, You Are A Number Wizard!";
+        } else if (usersGuess > guessNumber) {
+            infoText.text = "Your guess is too BIG, try a smaller number.";
+        } else if (usersGuess < guessNumber) {
+            infoText.text = "Your guess is too SMALL, try a bigger number.";
+        } else {
+            infoText.text = "Please Enter Your Guess To Play";
+        }
+    }
+}
